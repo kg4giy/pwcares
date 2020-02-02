5f6b8b2b0606bba1a59ccee98c16e95dfd17a357
David A. Lane <kg4giy@gmail.com>
2020-02-02 11:59:00 -0500

    Updated training.sql for additional courses and renumbered courses.

--- sql/training.sql
+++ sql/training.sql
@@ -23,6 +23,7 @@ CREATE TABLE training
   is_400 VARCHAR(1),
   is_706 VARCHAR(1),
   is_546 VARCHAR(1),
+  is_1300 VARCHAR(1),
   eoc_101 VARCHAR(1),
   g_191 VARCHAR(1),
   g_775 VARCHAR(1),
@@ -30,14 +31,16 @@ CREATE TABLE training
   is_703 VARCHAR(1),
   is_120 VARCHAR(1),
   is_230 VARCHAR(1),
+  is_235 VARCHAR(1),
   is_240 VARCHAR(1),
   is_241 VARCHAR(1),
   is_242 VARCHAR(1),
   is_244 VARCHAR(1),
   is_288 VARCHAR(1),
+  is_2200 VARCHAR(1),
   e_969 VARCHAR(1),
   is_29 VARCHAR(1),
-  ec-016 VARCHAR(1)
+  ec_016 VARCHAR(1)
 );
 
 
@@ -49,7 +52,7 @@ FIELDS TERMINATED BY ',';
 
 //Join query to select entries for reporting
 
-SELECT pwcares.call_sign.call_sign, pwcares.call_sign.last, pwcares.call_sign.first, pwcares.call_sign.appointment, pwcares.training.is_100, pwcares.training.is_200,pwcares.training.is_300, pwcares.training.is_400, pwcares.training.is_700, pwcares.training.is_800, pwcares.training.g_191, pwcares.training.g_775, pwcares.training.eoc_101  
+SELECT pwcares.call_sign.call_sign, pwcares.call_sign.last, pwcares.call_sign.first, pwcares.call_sign.appointment, pwcares.training.is_100, pwcares.training.is_200,pwcares.training.is_300, pwcares.training.is_400, pwcares.training.is_700, pwcares.training.is_800, pwcares.training.is_1300, pwcares.training.g_191, pwcares.training.g_775, pwcares.training.eoc_101 
 INTO OUTFILE '/tmp/training_status.txt' 
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n'
 FROM pwcares.call_sign right join pwcares.training on pwcares.call_sign.call_sign_id=pwcares.training.train_id WHERE pwcares.call_sign.status = 'A';
