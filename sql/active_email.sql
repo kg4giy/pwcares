a62750e8fb47a485af2eba6f7d3010ea8f942a97
David A. Lane <kg4giy@arranmore.fios-router.home>
2021-01-14 10:08:35 -0500

    New join sql for pulling active email addresses.

---
+++ sql/active_email.sql
@@ -0,0 +1,9 @@
+//Active Email
+
+use pwcares;
+
+SELECT pwcares.call_sign.call_sign, pwcares.home_address.email1
+INTO OUTFILE '/tmp/training_status.txt' 
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n'
+FROM pwcares.call_sign right join pwcares.home_address on pwcares.call_sign.call_sign_id=pwcares.home_address.home_id WHERE pwcares.call_sign.status = 'A';
+
