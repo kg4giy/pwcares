d81ab92820c0eecc3c4773fc091b9926e7023a2f
David A. Lane <kg4giy@gmail.com>
2023-03-15 15:55:15 -0400

    Updated the outline of active email.

--- sql/active_email.sql
+++ sql/active_email.sql
@@ -3,7 +3,7 @@
 use pwcares;
 
 SELECT pwcares.call_sign.call_sign, pwcares.home_address.email1
-INTO OUTFILE '/tmp/training_status.txt' 
+INTO OUTFILE '/tmp/email_status.txt' 
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n'
 FROM pwcares.call_sign right join pwcares.home_address on pwcares.call_sign.call_sign_id=pwcares.home_address.home_id WHERE pwcares.call_sign.status = 'A';
 
